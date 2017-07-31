5b492dae8f004820abda45562368f9eeb3e77d0f
nctson <chieuthanhson.nguyen@gmail.com>
2017-07-31 20:12:34 +0700

    test
    test commit

---
+++ BasicWebCrawler/BasicWebCrawler/Object/Earnings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebCrawler.Object
+{
+    class Earnings
+    {
+        public String Symbol { get; set; }
+        public String Company { get; set; }
+        public String MarketCap { get; set; }
+        public float Estimate { get; set; }
+        public float Reported { get; set; }
+        public String Surprise { get; set; }
+        public String Surp { get; set; }
+        public String PriceChange { get; set; }
+        public String Report { get; set; }
+
+    }
+}
