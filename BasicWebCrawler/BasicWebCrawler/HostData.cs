9ad913fd849b22204e24d9ae4a9ed8926d087f75
nctson <chieuthanhson.nguyen@gmail.com>
2017-07-31 23:24:33 +0700

    test get data

---
+++ BasicWebCrawler/BasicWebCrawler/HostData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace BasicWebCrawler
+{
+    class HostData
+    {
+        public string getJsonString(string url)
+        {
+            var json = new WebClient().DownloadString(url);
+            return json.ToString();
+        }
+
+    }
+}
