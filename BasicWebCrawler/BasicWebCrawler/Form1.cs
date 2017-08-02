4993ab68b78cae98f3deeee243272b419abe4d53
nttien <30600894+nttien@users.noreply.github.com>
2017-08-02 18:20:40 +0700

    changed code Form1

--- BasicWebCrawler/BasicWebCrawler/Form1.cs
+++ BasicWebCrawler/BasicWebCrawler/Form1.cs
@@ -9,8 +9,9 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BasicWebCrawler.Object;
-
-using BasicWebCrawler.Object;
+using Quartz;
+using System.Threading;
+using Quartz.Impl;
 
 namespace BasicWebCrawler
 {
@@ -23,14 +24,30 @@ namespace BasicWebCrawler
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            try
+            {
+                // Grab the Scheduler instance from the Factory 
+                IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+                // and start it off
+                scheduler.Start();
+
+                // some sleep to show what's happening
+                Thread.Sleep(TimeSpan.FromSeconds(60));
+
+                // and last shut down the scheduler when you are ready to close your program
+                scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
         }
 
         private void btnGetData_Click(object sender, EventArgs e)
         {
             HostData data = new HostData();
             data.GetData();
-
         }
 
     }
