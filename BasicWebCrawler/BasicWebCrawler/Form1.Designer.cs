3d6fd38680029cfe0480c8c8b9db625e8c0db2bf
nttien <30600894+nttien@users.noreply.github.com>
2017-08-02 18:10:34 +0700

    changed UI design
    change Form1

--- BasicWebCrawler/BasicWebCrawler/Form1.Designer.cs
+++ BasicWebCrawler/BasicWebCrawler/Form1.Designer.cs
@@ -29,33 +29,71 @@
         private void InitializeComponent()
         {
             this.btnGetData = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Respond = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnGetData
             // 
-            this.btnGetData.Location = new System.Drawing.Point(99, 148);
+            this.btnGetData.Location = new System.Drawing.Point(68, 86);
             this.btnGetData.Name = "btnGetData";
-            this.btnGetData.Size = new System.Drawing.Size(75, 23);
+            this.btnGetData.Size = new System.Drawing.Size(350, 23);
             this.btnGetData.TabIndex = 1;
             this.btnGetData.Text = "Get Update";
             this.btnGetData.UseVisualStyleBackColor = true;
             this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Respond";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Web Crawler";
+            // 
+            // txt_Respond
+            // 
+            this.txt_Respond.Location = new System.Drawing.Point(68, 60);
+            this.txt_Respond.Name = "txt_Respond";
+            this.txt_Respond.Size = new System.Drawing.Size(350, 20);
+            this.txt_Respond.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(433, 122);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Respond);
             this.Controls.Add(this.btnGetData);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Basic Web Crawler";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Respond;
     }
 }
 
