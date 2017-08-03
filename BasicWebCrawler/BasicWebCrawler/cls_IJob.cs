477ecad3b9ed7b36589363e0c8529070076bd783
nttien <30600894+nttien@users.noreply.github.com>
2017-08-03 13:01:09 +0700

    changed

--- BasicWebCrawler/BasicWebCrawler/cls_IJob.cs
+++ BasicWebCrawler/BasicWebCrawler/cls_IJob.cs
@@ -7,25 +7,21 @@ using Quartz.Impl;
 
 namespace BasicWebCrawler
 {
-    public class cls_IJob
+    public class cls_IJob : IJob
     {
+        //Get Scheduler
         public static void GetData(string _text)
         {
             try
             {
-                // Grab the Scheduler instance from the Factory 
                 IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
-
-                // and start it off
                 scheduler.Start();
 
-                // define the job and tie it to our HelloJob class
-                IJobDetail job = JobBuilder.Create<HelloJob>()
+                IJobDetail job = JobBuilder.Create<cls_IJob>()
                     .WithIdentity("job1", "group1")
                     .UsingJobData("Name", _text)
                     .Build();
 
-                // Trigger the job to run now, and then repeat every 10 seconds
                 ITrigger trigger = TriggerBuilder.Create()
                     .WithSimpleSchedule(x => x
                         .WithIntervalInSeconds(10)
@@ -33,13 +29,8 @@ namespace BasicWebCrawler
                     .StartNow()
                     .Build();
 
-                // Tell quartz to schedule the job using our trigger
                 scheduler.ScheduleJob(job, trigger);
-
-                // some sleep to show what's happening
                 Thread.Sleep(TimeSpan.FromSeconds(30));
-
-                // and last shut down the scheduler when you are ready to close your program
                 scheduler.Shutdown();
             }
             catch (SchedulerException se)
@@ -58,13 +49,10 @@ namespace BasicWebCrawler
         }
 
         //Create class inheritance
-        class HelloJob : IJob
+        public void Execute(IJobExecutionContext context)
         {
-            public void Execute(IJobExecutionContext context)
-            {
-                var _name = context.JobDetail.JobDataMap.GetString("Name");
-                WriteLogs(DateTime.Now.ToString() + ": " + _name);
-            }
+            var _name = context.JobDetail.JobDataMap.GetString("Name");
+            WriteLogs(DateTime.Now.ToString() + ": " + _name);
         }
     }
 }
